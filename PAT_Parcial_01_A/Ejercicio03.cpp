6de925e5feb8e10fe2f53096193a7675a846c90e
DavidLepe31 <d.alconerolepe@gmail.com>
2023-10-27 06:52:46 -0600

    nuevo intento34

--- PAT_Parcial_01_A/Ejercicio03.cpp
+++ PAT_Parcial_01_A/Ejercicio03.cpp
@@ -7,23 +7,27 @@ MinStack::MinStack() {
 }
 
 void MinStack::push(int value) {
-    Node<int>* tmp = new Node<int>(value, headatos);
+    Node<int>* tmp = new Node<int>();
+    tmp->value = value;
+    tmp->next = headatos;
     // Agregar el valor a la pila de datos.
-    if (headstack == nullptr) {
+    if (headatos== nullptr) {
         headatos=tmp;
         headmin = tmp;
     }
     headatos = tmp;
-    datastack = datastack->next;
+
     if (value <= headmin->value || headmin == nullptr) {
-        Node<int>* tmp2 = new Node<int>(value, headmin);
+        Node<int>* tmp2 = new Node<int>();
+        tmp2->value = value;
+        tmp2->next = headmin;
         headmin = tmp2;
     }
 
 }
 void MinStack::pop() {
     if (headatos = nullptr) {
-        return INT_MIN;
+        return;
     }
     
         // Si el valor que se va a eliminar es el valor mínimo, eliminarlo de la pila de seguimiento de mínimos.
