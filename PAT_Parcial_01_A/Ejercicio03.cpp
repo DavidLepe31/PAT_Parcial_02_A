6eeef4c4c994f007794ad56ab0e0f1dfb217fe64
DavidLepe31 <d.alconerolepe@gmail.com>
2023-10-30 21:29:20 -0600

    comprobacoin aceptada

--- PAT_Parcial_01_A/Ejercicio03.cpp
+++ PAT_Parcial_01_A/Ejercicio03.cpp
@@ -1,60 +1,45 @@
 #include "Ejercicio03.h"
-#include "Node.h"
-#include<climits>
+#include <algorithm> 
 
-MinStack::MinStack() {
-
+MinStack::Node::Node(int value, int minimum, Node* nxt) { //constructor de nodo
+	element = value;
+	minimo = minimum;
+	next = nxt;
 }
 
-void MinStack::push(int value) {
-    Node<int>* tmp = new Node<int>();
-    tmp->value = value;
-    tmp->next = headatos;
-    // Agregar el valor a la pila de datos.
-    if (headatos== nullptr) {
-        headatos=tmp;
-        headmin = tmp;
-    }
-    headatos = tmp;
-
-    if (value <= headmin->value || headmin == nullptr) {
-        Node<int>* tmp2 = new Node<int>();
-        tmp2->value = value;
-        tmp2->next = headmin;
-        headmin = tmp2;
-    }
-
-}
-void MinStack::pop() {
-    if (headatos = nullptr) {
-        return;
-    }
-    
-        // Si el valor que se va a eliminar es el valor mínimo, eliminarlo de la pila de seguimiento de mínimos.
-        if (top() == getMin()) {
-            Node<int>* node = headmin;
-            headmin = headmin->next;
-            delete node;
-        }
-
-        Node<int>* tmpp = headatos;
-        headatos = headatos->next;
-        delete tmpp;
-       
-    
+MinStack::MinStack()
+{
+	head = nullptr;
 }
 
-
-
-int MinStack::top() {
-
-    return headatos->value;
+void MinStack::push(int value)
+{
+	if (head == nullptr) {
+		head = new Node(value, value, nullptr);
+	}
+	else {
+		head = new Node(value, std::min(head->minimo, value), head);
+	}
 }
 
-int MinStack::getMin() {
-    return headmin->value;
+void MinStack::pop()
+{
+	if (head != nullptr) {
+		Node* auxiliar = head;
+		head = head->next;
+		delete auxiliar;
+	}
 }
 
+int MinStack::top()
+{
+	return head->element;
+}
+
+int MinStack::getMin()
+{
+	return head->minimo;
+}
 
 
 
--- PAT_Parcial_01_A/Ejercicio03.h
+++ PAT_Parcial_01_A/Ejercicio03.h
@@ -1,19 +1,27 @@
-
 #pragma once
-#include "Node.h"
-
 class MinStack
 {
-public:
-    Node<int>* headmin = nullptr;
-    Node<int>* headatos = nullptr;
-    
-   
-    MinStack();
-    void push(int value);
-    void pop();
-    int top();
-    int getMin();
 
+private:
+	struct Node {
+		int element;
+		int minimo;
+		Node* next;
+
+		Node(int value, int minimum, Node* nxt);
+	};
+
+	Node* head;
+
+public:
+	MinStack();
+
+	void push(int value);
+
+	void pop();
+
+	int top();
+
+	int getMin();
 };
 
